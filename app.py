64e4957615529c2b27e4b64dc097400c638a88d5
gajiann <ghazyrafifpd@gmail.com>
2026-08-29 01:49:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ api_key = st.sidebar.text_input("Masukkan Gemini API Key:", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-pro')
 
     # Fitur Upload Gambar
     uploaded_file = st.sidebar.file_uploader("Unggah Foto (Opsional):", type=["jpg", "jpeg", "png"])
